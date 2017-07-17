c1989d601456318ed06ddca6a51297475281b9fa
Sujeet Sukumaran <ssukumaran@utah.gov>
2017-07-17 15:33:57 -0600

    first steps of WYAS

---
+++ wyas/FirstSteps/FirstSteps.hs
@@ -0,0 +1,36 @@
+-- to compile:
+-- > stack ghc FirstSteps.hs
+
+-- to run
+-- > stack runghc FirstSteps.hs
+
+module FirstSteps where
+import System.Environment
+
+
+main = do
+   -- Ex 1
+   -- reads two arguments from the command line, and prints out a message using both of them
+   args <- getArgs
+   let x = args !! 0
+   let y = args !! 1
+   putStrLn ("Hello, " ++ x ++ " and " ++ y)
+
+   -- Ex 2
+   -- Change the program so it performs a simple arithmetic operation on the two
+   -- arguments and prints out the result. You can use read to convert a string
+   -- to a number, and show to convert a number back into a string.
+   -- Play around with different operations.
+   let n1 = read (args !! 2)
+   let g = args !! 3
+   let n2 = read g
+   putStrLn ("Oh, and " ++ (show n1) ++ " + " ++ (show n2) ++ " is " ++ (show (n1 + n2)))
+
+   -- Ex 3
+   -- getLine is an IO action that reads a line from the console and returns it as a string.
+   -- Change the program so it prompts for a name, reads the name, and then prints that
+   -- instead of the command line value
+   putStrLn ("Gimme> ")
+   line <- getLine
+   putStrLn ("You entered " ++ line)
+
---
+++ wyas/README.md
@@ -0,0 +1,7 @@
+(Original)
+	https://en.wikibooks.org/wiki/Write_Yourself_a_Scheme_in_48_Hours
+(version 2.0)
+	https://github.com/write-you-a-scheme-v2/scheme
+	AND https://wespiser.com/writings/wyas/00_overview.html
+
+chapter 1 - First Steps  (from Original)
